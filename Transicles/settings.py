9bd960c60aa3e36b4a25f94d2ffb5166a107ec90
Usman Ali <bad199614@gmail.com>
2017-09-07 21:03:21 +0500

    Added Pictures and WebDetective 1.0

--- Transicles/settings.py
+++ Transicles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4n@_lr5wzc%*47dr^u6!sk824+xm*0gd6l!np5(oo+6-z6r)fp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','transicles.pythonanywhere.com']
 
